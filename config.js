98f9300e076558c29176cba8feeb928ac8e27632
ronnycarlsen <ronny.carlsen@cutrin-norge.no>
2026-05-23 12:35:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Fyll inn disse to fra Supabase når databasen er laget.
 // La stå tomt for å teste lokalt med lagring i nettleseren.
 window.STAFETT_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://jkoxdjcdhgayjieqqioo.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imprb3hkamNkaGdheWppZXFxaW9vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTUxMTYxOSwiZXhwIjoyMDk1MDg3NjE5fQ.7jehVgG9klclaFM-0_NlqmKSyGEPZZ3MI1g44BVbcJM",
   EVENT_ID: "trondheimstafetten-2026"
 };
